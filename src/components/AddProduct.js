ace30a0e431baa5c5b746cb16600f494b66800db
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2022-01-01 20:36:53 +0700

    post data

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,9 +1,23 @@
-import React from "react";
-
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 export const AddProduct = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const history = useHistory();
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5000/products", {
+      title: title,
+      price: price,
+    });
+    history.push("/");
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={saveProduct}>
         <div className="field">
           <label className="label">Title</label>
           <input
@@ -11,6 +25,8 @@ export const AddProduct = () => {
             type="text"
             name="title"
             placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="field">
@@ -20,6 +36,8 @@ export const AddProduct = () => {
             type="text"
             name="price"
             placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
           />
         </div>
         <div className="field">
